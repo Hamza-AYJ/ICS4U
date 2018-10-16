13d5823b8d905f5108a0ccf5ab18eda1681e8373
Andy <43015964+andyung17@users.noreply.github.com>
2018-10-16 10:29:37 -0400

    Added Dinner file
    Code containing cheese

---
+++ js/Dinner.js
@@ -0,0 +1,24 @@
+background(255, 255, 196); // wooden table
+fill(177, 181, 179);//plate color
+ellipse(200, 200, 350, 350); // plate
+ellipse(200, 200, 300, 300); 
+
+//Cheese color
+fill(252, 252, 5);
+//Thicking lines
+strokeWeight(2);
+//Outer lines color
+stroke(8, 8, 8);
+//Triangle
+triangle (294,145,105,145,158,72);
+//Rectangle
+rect(103,144,192,160);
+//circle color
+fill(216, 239, 240);
+//circles
+ellipse(138,259,50,50);
+ellipse(263,179,50,50);
+ellipse(256,259,25,25);
+ellipse(206,259,10,10);
+ellipse(139,187,15,15);
+ellipse(172,111,35,35);
