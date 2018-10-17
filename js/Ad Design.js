0f00e31fc9b90ada9ebb8a987332a5fa61b97e0a
Andy <43015964+andyung17@users.noreply.github.com>
2018-10-17 11:50:50 -0400

    Create Ad Design.js

---
+++ js/Ad Design.js
@@ -0,0 +1,26 @@
+noStroke();
+background(0, 255, 89);//Sets background colour
+fill(255, 0, 0);//Setting colour to text
+textSize(30);//Setting text size
+text("Emoji Winston Pillow!", 10, 30);//Heading
+textSize(12);//Setting text size to 12 
+text("So incredibly cute and fluffy, you'll never find any other pillow like this!", 10, 68);
+fill(245, 245, 5);//Setting to a colour
+ellipse(179,251,250,250);//Drawing a circle
+fill(255, 0, 0);//Colouring circle
+ellipse(228,294,94,95);//Creating circle
+fill(0, 0, 0);//Colouring
+ellipse(257,210,30,30);//Creating circle
+ellipse(147,210,30,30);//Creating circle
+rotate(18);//Rotating
+fill(17, 0, 255);//Setting colour
+textSize(50);//Changing text size
+text("Soft", 390, 252);//Adding text
+fill(17, 0, 255);//Setting colour
+textSize(45);//Setting text size
+text("Fluffy", 289, 43);//Adding text
+rotate(-50);//Rotating
+fill(17, 0, 255);//Setting colour
+textSize(35);//Setting text size
+text("Cuddly", -80, 172);//Adding text
+
